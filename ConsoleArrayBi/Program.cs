a2d5c177a81c6674a097c379e8833812857f834c
Jalin Habei <Jalin.Habei@gmail.com>
2019-07-10 10:57:11 -0300

    ArrayBi

---
+++ ConsoleArrayBi/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleArrayBi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[,] listaDeLivros = new string[2, 2]
+                {{"O Peuqeno","sim"},{"O Grande","não"}};
+
+            for (int i = 0; i < listaDeLivros.GetLength(0); i++)
+
+            {
+                var Nomelivro = listaDeLivros[i, 0];
+                var disponivel = listaDeLivros[i, 1];
+
+                Console.WriteLine($"Nome do livro:{Nomelivro} disponivel:{disponivel}.");
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
