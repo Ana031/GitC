6371caaf2e4b4c21113906e2eaec53c4144e402b
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-17 17:28:36 -0300

    FoodTruckSQL

---
+++ SQL/FoodTruck.sql
@@ -0,0 +1,10 @@
+﻿select
+           Tipo,
+           TotalVendas,
+	      month (DataVenda),
+		   (SUM(TotalVendas) )as 'Media mensal',
+		month(DataVenda) as 'Mes'
+		
+from FoodTruck
+group by Tipo,TotalVendas,month(DataVenda)
+Order by TotalVendas desc
---
+++ SQL/UPDATE.sql
@@ -0,0 +1,7 @@
+﻿select *from AulaCsharp ;
+  update AulaCsharp
+  set Aluno = 'Robson',
+      Idade = 31
+	  where Id = 25;
+
+Select *from AulaCsharp;
