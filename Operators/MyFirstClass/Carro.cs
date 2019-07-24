fb76895f3a5a9fe013d1f3dd593b9a7f5120c9b9
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-24 08:08:21 -0300

    QueryInnerJoin.Completo

---
+++ Operators/MyFirstClass/Carro.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators.MyFirstClass
+{
+    class Carro
+    {
+                public int Rodas { get; set; } = 0;
+                public int Portas { get; set; } = 0;
+                public int Bancos { get; set; } = 0;
+                public int Janelas { get; set; } = 0;
+
+        public void MostrarCarro()
+        {
+            Console.WriteLine($"Nosso Carro: \r\n" +
+                $"Quantidade Rodas[{Rodas}] \r\n" +
+                $"Quantidade Portas[{Portas}] \r\n" +
+                $"Quantidade Bancos[{Bancos}] \r\n" +
+                $"Quantidade Janelas[{Janelas}]");
+        }
+    }
+}
---
+++ Operators/Program.cs
@@ -0,0 +1,22 @@
+﻿using Operators.MyFirstClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var teste = new Carro();
+
+            teste.MostrarCarro();
+
+            Console.ReadKey();
+        }
+   
+    }
+}
