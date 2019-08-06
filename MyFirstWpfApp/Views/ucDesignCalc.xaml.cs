6cea836615fe30c7d93c7c661c9b0cbde76d4f27
Treinamento HBSIS <treinamento@hbsis.com.br>
2019-08-06 17:31:27 -0300

    ommit first app

---
+++ MyFirstWpfApp/Views/ucDesignCalc.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyFirstWpfApp.Views
+{
+    /// <summary>
+    /// Interação lógica para ucDesignCalc.xam
+    /// </summary>
+    public partial class ucDesignCalc : UserControl
+    {
+        public ucDesignCalc()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
